69aad198b271a313d5da047fb075f4df55d4a94f
John <johnmulvey48@yahoo.com>
2022-08-10 12:29:22 -0400

    git@github.com:Jmulvez/phase-3-ruby-oo-self-person-lab.git

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
-# your code goes here
+class Person
+    attr_accessor :name, :bank_account, :happiness, :hygiene
+    def name(name, bank_account, happiness, hygiene)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+    def clean
+        if hygiene > 7
+            puts true
+        end
+    def happy
+        if happiness > 7
+            puts true
+        end
+end
